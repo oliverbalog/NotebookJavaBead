e2a7fa0c41bb119d01891ee679dea7728904aeaa
oliverbalog <asperant8@gmail.com>
2022-10-18 19:11:01 +0200

    s

---
+++ src/main/java/com/example/notebookjavabead/NotebookJavaBeadApplication.java
@@ -0,0 +1,13 @@
+package com.example.notebookjavabead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookJavaBeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookJavaBeadApplication.class, args);
+    }
+
+}
